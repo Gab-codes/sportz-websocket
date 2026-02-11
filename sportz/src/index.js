b166d1342a83813e261292dcecf55422c1375ffb
Temidayo Gabriel <dayotoru@gmail.com>
2026-02-11 08:37:57 -0500

    implement matches POST and GET route

--- sportz/src/index.js
+++ sportz/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
 const PORT = 8000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send({ message: "Hello from express server!" });
 });
+
+app.use("/matches", matchRouter);
 
 // Listen on port 8000 and log the URL when the server starts
 app.listen(PORT, () => {
