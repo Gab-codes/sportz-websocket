0d14f06ae2e6a4e799dee705e3e307d2b6e7bacd
Temidayo Gabriel <dayotoru@gmail.com>
2026-02-11 19:00:11 -0500

    implement pub-sub broadcasting for commentary

--- sportz/src/index.js
+++ sportz/src/index.js
@@ -3,6 +3,7 @@ import { matchRouter } from "./routes/matches.js";
 import http from "http";
 import { attachWebSocketServer } from "./ws/server.js";
 import { securityMiddleware } from "./arcjet.js";
+import { commentaryRouter } from "./routes/commentary.js";
 
 const app = express();
 const PORT = Number(process.env.PORT || 8000);
@@ -20,9 +21,12 @@ app.get("/", (req, res) => {
 app.use(securityMiddleware());
 
 app.use("/matches", matchRouter);
+app.use("/matches/:id/commentary", commentaryRouter);
 
-const { broadcastMatchCreated } = attachWebSocketServer(server);
+const { broadcastMatchCreated, broadcastCommentary } =
+  attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
 
 // Listen on port 8000 and log the URL when the server starts
 server.listen(PORT, HOST, () => {
